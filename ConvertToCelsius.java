fda6b042bdd4f465f7a2fcd97e92f4c168a2b305
Svitlana_Kunytska <balbesca1988@gmail.com>
2024-08-17 13:03:38 +0300

    Homework Lesson 1

---
+++ ConvertToCelsius.java
@@ -0,0 +1,6 @@
+public class ConvertToCelsius {
+    public static double convertToCelsius(double degreeFahrenheit)
+    {
+        return (5.0 / 9.0) * (degreeFahrenheit - 32);
+    }
+}
---
+++ Converter1.java
@@ -0,0 +1,7 @@
+public class Converter1 {
+
+    public static double inchesToCentimeters(double inches) {
+        double conversionFactor = 2.54;
+        return inches * conversionFactor;
+    }
+}
